bb20849da748ac06dd5d39a5138ea414c299bbe9
ibnu haskoro cintantyo <ibnuhaskoro91@gmail.com>
2025-05-07 14:25:44 +0700

    Deskripsi perubahan

--- api/helpers/vite.ts
+++ api/helpers/vite.ts
@@ -3,7 +3,7 @@ import fs from "fs";
 import path from "path";
 import { createServer as createViteServer, createLogger } from "vite";
 import { type Server } from "http";
-import viteConfig from "../vite.config";
+import viteConfig from "../../vite.config";
 import { nanoid } from "nanoid";
 import type { ServerOptions } from "vite";
 
@@ -50,7 +50,7 @@ export async function setupVite(app: Express, server: Server) {
     try {
       const clientTemplate = path.resolve(
         import.meta.dirname,
-        "..",
+        "../../",
         "client",
         "index.html",
       );
